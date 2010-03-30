8db641b4bcb9034ed866b04c8c97bf5885c83cf3
Pavel Yaskevich <povel.y@gmail.com>
2010-03-30 17:22:45 +0300

    font and background colour for labels + update functionality.

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -5,6 +5,15 @@ class LabelsController < ApplicationController
 #     @labels = Label.find_all_by_global(true)
 #   end
   
+  def update
+    @label = Label.find_by_id(params[:id])
+    @label.update_attributes(params[:label])
+
+    respond_to do |format|
+      format.js
+    end
+  end
+  
   def add_issue
     @label = Label.find_by_id(params[:id])
     @issue = Issue.find_by_id(params[:issue_id])
--- db/migrate/20100329104058_create_labels.rb
+++ db/migrate/20100329104058_create_labels.rb
@@ -3,12 +3,10 @@ class CreateLabels < ActiveRecord::Migration
     create_table :labels do |t|
 
       t.column :title, :string
-
       t.column :user_id, :integer
-
       t.column :global, :boolean, :default => false, :null => false
-
-      t.column :color, :string, :default => '#ffffff', :limit => 7
+      t.column :fncolor, :string, :default => '#000000', :limit => 7
+      t.column :bgcolor, :string, :default => '#ffffff', :limit => 7
       
     end
   end
