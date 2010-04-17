cceb3a78c9a01a7b388829302235149f49bbad29
uzzz <uzzable@gmail.com>
2010-04-17 13:59:23 +0300

    multiple drag and drop added

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -33,16 +33,18 @@ class LabelsController < ApplicationController
 
   def link
     @label = Label.find_by_id(params[:id])
-    @issue = Issue.find_by_id(params[:issue_id])
+    @issues = Issue.find(params[:issue_ids])
     
     
-    if @label && @issue
-      if @label.issues_add(@issue)
-        labels_custom_field = @issue.available_custom_fields.detect { |cf| cf.name.downcase == 'label' }
-        @issue.custom_field_values
-        labels_custom_field_value = @issue.custom_value_for(labels_custom_field)
-        labels_custom_field_value.value = labels_custom_field_value.value.to_s + "|#{@label.id}| "
-        labels_custom_field_value.save
+    if @label
+      @issues.each do |issue|
+        if @label.issues_add(issue)
+          labels_custom_field = issue.available_custom_fields.detect { |cf| cf.name.downcase == 'label' }
+          issue.custom_field_values
+          labels_custom_field_value = issue.custom_value_for(labels_custom_field)
+          labels_custom_field_value.value = labels_custom_field_value.value.to_s + "|#{@label.id}| "
+          labels_custom_field_value.save
+        end
       end
     end
 
